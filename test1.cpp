6233febea2c35329f60cd52d9b5c5543b7f6cc69
zhumingwei <zhumingwei2016@gmail.com>
2020-05-10 17:29:11 +0800

    format

--- test1.cpp
+++ test1.cpp
@@ -1,47 +1,50 @@
 // #include <string.h>
 #include <iostream>
 #include <vector>
-using std::string;
 using std::cout;
 using std::endl;
+using std::string;
 using std::vector;
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int i =1,j=2;
-    double slope = static_cast<double>(j) /i;
-    cout <<slope;
-
-    
+    int i = 1, j = 2;
+    double slope = static_cast<double>(j) / i;
+    cout << slope;
 }
 
-class Account {
-    public :
-        void calculate() {
-            amount += amount * interestRate;
-        }
-        static double rate() { return interestRate ;}
-        static void rate(double);
-        
-    private :
-        std::string owner;
-        double amount;
-        static double interestRate;
-        static double initRate();
+class Account
+{
+public:
+    void calculate()
+    {
+        amount += amount * interestRate;
+    }
+    static double rate() { return interestRate; }
+    static void rate(double);
+
+private:
+    std::string owner;
+    double amount;
+    static double interestRate;
+    static double initRate();
 };
 
-void Account::rate(double newRate){
+void Account::rate(double newRate)
+{
     interestRate = newRate;
 }
 
-class Example {
-    public :
+class Example
+{
+public:
     static double rate;
     static const int vecsize = 20;
     static vector<double> vec(int);
 };
 
 double Example::rate;
-vector<double> Example::vec(int v){
+vector<double> Example::vec(int v)
+{
     rate = v;
 }
